fcd580820c4298ecf08e9569cc41fce738c31cde
kidanemriam <kide.fikadu1@gmail.com>
2024-02-02 16:55:59 +0300

    bug fix

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -125,7 +125,7 @@
                                         class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">Users</a>
                                 </li>
                                 <li>
-                                    <a href="/users"
+                                    <a href="/invitation"
                                         class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">Invitations</a>
                                 </li>
                             @endif
