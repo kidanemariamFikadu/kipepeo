18f48243ff6729207d549c1dead9fc8dd898d239
kidanemriam <kide.fikadu1@gmail.com>
2024-02-03 16:04:46 +0300

    logo added

--- resources/views/livewire/setting/index.blade.php
+++ resources/views/livewire/setting/index.blade.php
@@ -118,60 +118,63 @@
             {{ $this->schoolList->links() }}
         </div>
     </div>
-    <div class="flex mb-4">
-        <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 w-1/2 p-4 mr-4">
-            <!-- Modal header -->
-            <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
-                <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
-                    Job titles
-                </h3>
-            </div>
-            <!-- Modal body -->
-            <div>
-                <button wire:click="$dispatch('openModal', { component: 'setting.job-title' })"
-                    class="px-3 py-1 bg-teal-500 text-white rounded mb-4 mt-2">+ Add Job title</button>
-            </div>
-            <ul class="max-w-md divide-y divide-gray-200 dark:divide-gray-700 mt-2">
-                @foreach ($this->jobTitleList as $jobTitle)
-                    <li class="pb-3 sm:pb-4">
-                        <div class="flex items-center space-x-4 rtl:space-x-reverse">
-                            <div class="flex-1 min-w-0">
-                                <p class="text-sm font-medium text-gray-900 dark:text-white">
-                                    {{ $jobTitle->name }}
-                                </p>
-                            </div>
-                            <div class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
-                                <div class="inline-flex rounded-md shadow-sm" role="group">
-                                    <button title="edit job title"
-                                        class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
-                                        wire:click="$dispatch('openModal', { component: 'setting.job-title', arguments: { jobTitleId: {{ $jobTitle->id }} }})">
-                                        <svg class="h-5 w-5 text-teal-500" viewBox="0 0 24 24" stroke-width="2"
-                                            stroke="currentColor" fill="none" stroke-linecap="round"
-                                            stroke-linejoin="round">
-                                            <path stroke="none" d="M0 0h24v24H0z" />
-                                            <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
-                                            <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
-                                            <line x1="16" y1="5" x2="19" y2="8" />
-                                        </svg>
-                                    </button>
-                                    <button title="remove job title"
-                                        wire:confirm="You are about to delete this job title. Are you sure?"
-                                        class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
-                                        wire:click="removeJobTitle({{ $jobTitle->id }})">
-                                        <svg class="h-5 w-5 text-red-500" fill="none" viewBox="0 0 24 24"
-                                            stroke="currentColor">
-                                            <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
-                                                d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
-                                        </svg>
+    @if (Auth::user()->role == 'admin')
+        <div class="flex mb-4">
+            <div class="relative bg-white rounded-lg shadow dark:bg-gray-700 w-1/2 p-4 mr-4">
+                <!-- Modal header -->
+                <div class="flex items-center justify-between p-4 md:p-5 border-b rounded-t dark:border-gray-600">
+                    <h3 class="text-lg font-semibold text-gray-900 dark:text-white">
+                        Job titles
+                    </h3>
+                </div>
+                <!-- Modal body -->
+                <div>
+                    <button wire:click="$dispatch('openModal', { component: 'setting.job-title' })"
+                        class="px-3 py-1 bg-teal-500 text-white rounded mb-4 mt-2">+ Add Job title</button>
+                </div>
+                <ul class="max-w-md divide-y divide-gray-200 dark:divide-gray-700 mt-2">
+                    @foreach ($this->jobTitleList as $jobTitle)
+                        <li class="pb-3 sm:pb-4">
+                            <div class="flex items-center space-x-4 rtl:space-x-reverse">
+                                <div class="flex-1 min-w-0">
+                                    <p class="text-sm font-medium text-gray-900 dark:text-white">
+                                        {{ $jobTitle->name }}
+                                    </p>
+                                </div>
+                                <div
+                                    class="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
+                                    <div class="inline-flex rounded-md shadow-sm" role="group">
+                                        <button title="edit job title"
+                                            class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-s-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+                                            wire:click="$dispatch('openModal', { component: 'setting.job-title', arguments: { jobTitleId: {{ $jobTitle->id }} }})">
+                                            <svg class="h-5 w-5 text-teal-500" viewBox="0 0 24 24" stroke-width="2"
+                                                stroke="currentColor" fill="none" stroke-linecap="round"
+                                                stroke-linejoin="round">
+                                                <path stroke="none" d="M0 0h24v24H0z" />
+                                                <path d="M9 7 h-3a2 2 0 0 0 -2 2v9a2 2 0 0 0 2 2h9a2 2 0 0 0 2 -2v-3" />
+                                                <path d="M9 15h3l8.5 -8.5a1.5 1.5 0 0 0 -3 -3l-8.5 8.5v3" />
+                                                <line x1="16" y1="5" x2="19" y2="8" />
+                                            </svg>
+                                        </button>
+                                        <button title="remove job title"
+                                            wire:confirm="You are about to delete this job title. Are you sure?"
+                                            class="px-4 py-2 text-sm font-medium text-gray-900 bg-white border border-gray-200 rounded-e-lg hover:bg-gray-100 hover:text-blue-700 focus:z-10 focus:ring-2 focus:ring-blue-700 focus:text-blue-700 dark:bg-gray-700 dark:border-gray-600 dark:text-white dark:hover:text-white dark:hover:bg-gray-600 dark:focus:ring-blue-500 dark:focus:text-white"
+                                            wire:click="removeJobTitle({{ $jobTitle->id }})">
+                                            <svg class="h-5 w-5 text-red-500" fill="none" viewBox="0 0 24 24"
+                                                stroke="currentColor">
+                                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                                    d="M19 7l-.867 12.142A2 2 0 0116.138 21H7.862a2 2 0 01-1.995-1.858L5 7m5 4v6m4-6v6m1-10V4a1 1 0 00-1-1h-4a1 1 0 00-1 1v3M4 7h16" />
+                                            </svg>
 
-                                    </button>
+                                        </button>
+                                    </div>
                                 </div>
                             </div>
-                        </div>
-                    </li>
-                @endforeach
-            </ul>
-            {{ $this->schoolList->links() }}
+                        </li>
+                    @endforeach
+                </ul>
+                {{ $this->schoolList->links() }}
+            </div>
         </div>
-    </div>
+    @endif
 </div>
