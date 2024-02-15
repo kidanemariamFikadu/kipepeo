c5f668893b263adc82921c54b57b8cc97c7b7269
kidanemriam <kide.fikadu1@gmail.com>
2024-02-15 12:07:15 +0300

    menu update

--- resources/views/components/layouts/app.blade.php
+++ resources/views/components/layouts/app.blade.php
@@ -120,6 +120,10 @@
                                 <a href="#"
                                     class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">Report</a>
                             </li>
+                            <li>
+                                <a href="/data-entry"
+                                    class="block py-2 pr-4 pl-3 text-gray-700 rounded bg-primary-700 lg:bg-transparent lg:text-primary-700 lg:p-0 dark:text-white">Data Entry</a>
+                            </li>
                             @if (Auth::user()->role == 'admin')
                                 <li>
                                     <a href="/users"
